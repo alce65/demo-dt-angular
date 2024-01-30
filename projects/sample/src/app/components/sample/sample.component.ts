30d2aff1014c5b30b8829edf523940beee9cad80
alce65 <alce65@hotmail.es>
2024-01-30 19:26:29 +0100

    Add sample component

---
+++ projects/sample/src/app/components/sample/sample.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dit-sample',
+  standalone: true,
+  imports: [],
+  // templateUrl: './sample.component.html',
+  template: `
+    <h1>{{ title }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste vero sint
+      quam rerum necessitatibus odio atque expedita illo natus temporibus
+      nesciunt commodi doloremque hic, vel facere pariatur blanditiis omnis
+      voluptatum!
+    </p>
+  `,
+  // styleUrl: './sample.component.css',
+  styles: [
+    `
+      :host {
+        display: block;
+        padding: 1rem;
+        border: 2px solid #a28b8b;
+        margin: 1rem 0;
+      }
+    `,
+  ],
+})
+export class SampleComponent {
+  // Controller
+  title = 'Sample Component';
+}
