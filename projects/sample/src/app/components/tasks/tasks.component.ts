f77ede94a1c5e068ffdcdcc903b75d4a864710fc
alce65 <alce65@hotmail.es>
2024-02-02 20:22:33 +0100

    Update task component with add functionality

--- projects/sample/src/app/components/tasks/tasks.component.ts
+++ projects/sample/src/app/components/tasks/tasks.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Task } from '../../entities/task';
 import { getTasks } from '../../repo/task.mock.repo';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'dit-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
+  newTask: string = '';
+  @ViewChild('details', { static: true }) refDetails!: ElementRef;
+
+  constructor() {
+    console.log('Constructor', this.refDetails);
+  }
 
   async ngOnInit(): Promise<void> {
+    console.log('OnInit', this.refDetails);
     this.tasks = await getTasks();
   }
+
+  onAdd() {
+    const task: Task = {
+      id: crypto.randomUUID(),
+      title: this.newTask,
+      isDone: false,
+    };
+
+    this.tasks.push(task);
+    this.newTask = '';
+    console.log(this.tasks);
+    this.refDetails.nativeElement.open = false;
+  }
+
+  // onAddRef(value: string) {
+  //   console.log(value);
+  // }
 
   onChange(task: Task) {
     task.isDone = !task.isDone;
